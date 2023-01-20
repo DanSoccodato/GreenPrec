174b8304c9a89e674336bcbfa535df5969ca96a8
DanSoccodato <daniele.soccodato@gmail.com>
2023-01-20 17:08:05 +0100

    Diagonal elements: sum on loops works on test. Waiting for refactoring and definition change for forbidden_neighbours()

--- Preconditioners/Green.py
+++ Preconditioners/Green.py
@@ -18,6 +18,9 @@ def Green(H, order_diag, order_offdiag, max_length, Verbose, offdiag=False):
 
     st = time.time()
 
+    # TODO: Define forbidden_neighbours as a list and not as a np.empty().
+    #  Use the stash saved in git
+
     N = len(H)
     order_diag = min(order_diag, N)
     order_offdiag = min(order_offdiag, N)
@@ -38,13 +41,10 @@ def Green(H, order_diag, order_offdiag, max_length, Verbose, offdiag=False):
         # Diagonal elements
         # Initialize list of forbidden indices
         forbidden_neighbours = np.empty(order_diag, dtype=int)
-        orderCounter = 1
+        depth = 1
 
-        # Recursive computation of selfenergy for row i, up to user-defined order
-        # print(12* '-~' + ' Entering sum_on_neighbours for row i = {:} '.format(i) + 12* '-~' + '\n')
-        Sigma_i = sum_on_neighbours(H, i, forbidden_neighbours, orderCounter, order_diag)
-        # print(12* '-~' +' Exiting sum_on_neighbours. Sum is: {:.4f} '.format(Sigma_i)
-        #     + 12* '-~' + '\n\n')
+        #Sum over neighbours
+        Sigma_i = sum_on_neighbours(H, i, forbidden_neighbours, depth, order_diag)
 
         # Sum over loops
         for length in range(3, max_length + 1):
@@ -53,14 +53,25 @@ def Green(H, order_diag, order_offdiag, max_length, Verbose, offdiag=False):
                 i = loop[0]
                 j = 0
                 prod = 1
-                forbidden_index = []
 
-                while j < len(loop):
-                    forbidden_index.append(loop[j])
-                    prod = prod * H[loop[j], loop[j + 1]] * g_reduced(forbidden_index, loop[j + 1])
+                print("\nloop = {:}".format(loop))
+                forbidden_neighbours = np.empty(order_diag + len(loop) - 1, dtype=int)
+                while j < len(loop)-1:
+
+                    forbidden_neighbours[:j+1] = loop[:j+1]
+                    depth = 2 + j
+                    k = loop[j+1]
+
+                    sigma = sum_on_neighbours(H, k, forbidden_neighbours, depth, order_diag + j + 1, True)
+
+                    G_limited = 1. / (-H[k, k] - sigma)
+
+
+                    prod = prod * H[loop[j], k] * G_limited
                     j += 1
 
                 prod *= H[loop[-1], i]
+                Sigma_i += prod
 
         G0[i, i] = 1. / (-H[i, i] - Sigma_i)
 
@@ -70,10 +81,10 @@ def Green(H, order_diag, order_offdiag, max_length, Verbose, offdiag=False):
                 if j != i:  # Must exclude diagonal elements
                     forbidden_neighbours = np.empty((order_offdiag + 1), dtype=int)
                     forbidden_neighbours[0] = i
-                    orderCounter = 2
+                    depth = 2
 
                     i_Sigma_j = sum_on_neighbours(H, j, forbidden_neighbours,
-                                                  orderCounter, order_offdiag + 1)
+                                                  depth, order_offdiag + 1)
                     i_G_jj = 1. / (-H[j, j] - i_Sigma_j)
                     G0[i, j] = G0[i, i] * H[i, j] * i_G_jj
 
@@ -82,24 +93,22 @@ def Green(H, order_diag, order_offdiag, max_length, Verbose, offdiag=False):
     return G0
 
 
-def sum_on_neighbours(H, i, forbidden_neighbours, orderCounter, order):
+def sum_on_neighbours(H, i, forbidden_neighbours, depth, order, debug=False):
     sum_ = 0
-    forbidden_neighbours[orderCounter - 1] = i
+    forbidden_neighbours[depth - 1] = i
 
-    #TODO
-    # Temporary: find_cycles_recursive can also find neighbours.
-    # Use that instead of get_and_exclude_neighbours()
-    neighbours = get_and_exclude_neighbours(H, i, forbidden_neighbours, orderCounter)
+    neighbours = get_and_exclude_neighbours(H, i, forbidden_neighbours, depth)
 
-    # print('considered index: {}\tforbidden_neighbours: {}\tdepth: {}\tneighbour_list: {} '
-    #      .format(i, forbidden_neighbours[:orderCounter], orderCounter, neighbours))
+    if debug:
+        print("\ndepth = ", depth, " order = ", order)
+        print('considered index: {}\tforbidden_neighbours: {}\tdepth: {}\tneighbour_list: {} '
+              .format(i, forbidden_neighbours[:depth], depth, neighbours))
 
-    if orderCounter < order and len(neighbours) >= 1:
-        orderCounter += 1
-
+    if depth < order and len(neighbours) >= 1:
+        depth += 1
         for k in neighbours:
             sum_ += H[i, k] * 1. / (-H[k, k]
-                                    - sum_on_neighbours(H, k, forbidden_neighbours, orderCounter, order)
+                                    - sum_on_neighbours(H, k, forbidden_neighbours, depth, order, debug)
                                     ) * H[k, i]
 
     return sum_
